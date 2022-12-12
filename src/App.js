6af045bb33146e9ca839401b9342e7b867ee591e
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2022-12-13 06:46:19 +0900

    横NavBarを作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './layout/navigation/NavBar'
+import { Link } from "react-router-dom";
 
 
 function App() {
   return (
     <>
       <div>
-        Ant Design 実験場
+        <h1> Ant Design 実験場 </h1>
+        <h2> 各ページ </h2>
+          <Link to="/layout/navigation/navbar"> 横サイドバー  </Link>
       </div>
       <Routes>
           <Route path="/layout/navigation/navbar" element={<NavBar />} />
--- src/layout/navigation/NavBar.js
+++ src/layout/navigation/NavBar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { Menu } from 'antd';
 import 'antd/dist/reset.css';
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Content } = Layout;
 
 const NavBar = () => (
   <>
     <Layout>
-      <Header>Header</Header>
+      <Header>
+        <Menu mode="horizontal" >
+          <Menu.Item>
+            メニュー1
+          </Menu.Item>
+          <Menu.Item>
+            メニュー2
+          </Menu.Item>
+        </Menu>  
+      </Header>
       <Content>Content</Content>
-      <Footer>Footer</Footer>
     </Layout>
 
   </>
